3f5fc4b1f9e12ecb1ad2cde37d0b16531f2eac9c
Jesus-fran <jesusfran.dev@gmail.com>
2023-06-02 16:25:11 -0600

    se agrega una funcion de suma

--- index.php
+++ index.php
@@ -4,4 +4,10 @@ function restar($num1, $num2){
     return $num1-$num2;
 }
 
+// Se agrega esta nueva funcion
+function sumar($num1, $num2){
+    return $num1+$num2;
+}
+
 echo restar(5,5);
+echo sumar(2,4);
